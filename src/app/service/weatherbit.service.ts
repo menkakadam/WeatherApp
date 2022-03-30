df73c65fd8d7d0527983dc28c86be4b001dea168
Menka <menkabhujbal6@gmail.com>
2022-03-30 15:06:23 +0530

    Deploed updated.

--- src/app/service/weatherbit.service.ts
+++ src/app/service/weatherbit.service.ts
@@ -10,12 +10,8 @@ export class WeatherbitService {
   key: any = environment.key;
 
   constructor(private http: HttpClient) { }
-  // apiUrl: 'https://api.weatherbit.io/',
-  // key:'a91d62e27ef14cf78a3f12b1dd5fb86a'
 
   getByCityName(city: string){
-    // https://api.weatherbit.io/v2.0/current?lat=50&lon=40&key=a91d62e27ef14cf78a3f12b1dd5fb86a
-    // https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh&key=a91d62e27ef14cf78a3f12b1dd5fb86a
     return this.http.get(this.apiUrl+"v2.0/forecast/daily?city=" + city +"&key="+this.key)
   }
   
